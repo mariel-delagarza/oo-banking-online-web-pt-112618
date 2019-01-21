00c96fffcd3d44c5873497735a192f5105c8604b
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-01-21 18:47:32 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,6 +13,24 @@ class Transfer
     sender.valid? && receiver.valid?
   end
 
+  def execute_transaction 
+    if valid? && sender.balance > transfer_amount && self.status = "pending"
+      sender.balance -= amount
+      receiver.balance += amount 
+      self.status = "complete"
+    else 
+      reject_transfer
+    end 
+  end 
+
+  def reverse_transfer 
+    if valid? && sender.balance > transfer_amount && self.status = "complete"
+      sender.balance += amount
+      receiver.balance -= amount 
+      self.status = "reversed"
+    else 
+      reject_transfer
+    end 
 
 
 end
