7775655f7936d7fb377beb7abc7f79df0c4a21d4
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-01-21 18:37:41 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,21 +1,21 @@
 class Transfer
-  attr_accessor :status, :sender, :receiver, :transfer_amount 
+  attr_accessor :status, :sender, :receiver, :transfer_amount
 
   def initialize(status, sender, receiver, transfer_amount)
     @status = "pending"
-    @sender = sender 
-    @receiver = receiver 
+    @sender = sender
+    @receiver = receiver
     @transer_amount = 50
   end
 
   def valid?(account1, account2)
     if account1.valid? && account2.valid?
-      true 
-    else 
-      false 
-    end 
-  end 
+      true
+    else
+      false
+    end
+  end
 
-  
-  
+
+
 end
